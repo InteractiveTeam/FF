09a61d098584a0a3ba9426e540cf4aebdb8574b5
davinanaya <davinson.anaya@arkix.com>
2016-07-22 20:10:22 -0500

    initial commit

---
+++ js/ax-data.js
@@ -0,0 +1,106 @@
+var dataEvents = [
+    {
+        event:'Inauguración oficial-Festival de Orquestas',
+        date:'Julio 29',
+        hour:'6:00 p.m. a 1:00 a.m',
+        place:'Obelisco',
+        lat:6.2570995,
+        lng:-75.5943526
+    },
+    {
+        event:'Festival de la trova',
+        date:'Julio 29 a agosto 5',
+        hour:'6:00 p.m. a 1:00 a.m.',
+        place:'Parque Pies Descalzos',
+        lat:6.24499,
+        lng:-75.57738
+    },
+    {
+        event:'Parque Cultural Nocturno',
+        date:'Julio 31 a agosto 5',
+        hour:'6:00 p.m. a 1:00 a.m.',
+        place:'Plaza Gardel',
+        lat:6.2181482,
+        lng:-75.5869389
+    },
+    {
+        event:'La feria a ritmo de bicicleta',
+        date:'Julio 30',
+        hour:'2:00 p.m',
+        place:'Parque de las Luces',
+        lat:6.2458201,
+        lng:-75.5722863
+    },
+    {
+        event:'Desfile de Silleteritos',
+        date:'Julio 30',
+        hour:'Hora: 10:00 a.m. a 1:00 p.m.',
+        place:'Barrio La Floresta',
+        lat:6.257766,
+        lng:-75.598715
+    },
+    {
+        event:'Caminata canina y de mascotas',
+        date:'Julio 31',
+        hour:'8:00 a.m',
+        place:'Salida desde la estación Estadio',
+        lat:6.2531633,
+        lng:-75.588918
+    },
+    {
+        event:'Parque cultural. Noches con lo mejor del bolero, la música afro, colombiana y tropical',
+        date:'julio 31 y agosto 1 al 5',
+        hour:'',
+        place:'Plaza Gardel',
+        lat:6.2181482,
+        lng:-75.5869389
+    },
+    {
+        event:'Plaza de Flores. Muestra de cultura paisa con gastronomía, artesanía, música y juegos tradicionales',
+        date:'Agosto 2 al 6',
+        hour:'Martes y miércoles: 12 m. a 10 p.m. Jueves y sábado de 12 m. a 12 p.m.',
+        place:'Contiguo al Mamm (estación Industriales)',
+        lat:6.2243422,
+        lng:-75.574462
+    },
+    {
+        event:'Gospel Park',
+        date:'Julio 31',
+        hour:'1:00 a 11:00 p. m.',
+        place:'Parque de los Deseos (Estación Universidad)',
+        lat:6.2686003,
+        lng:-75.5665116
+    },
+    {
+        event:'Zona que Suena. Parque temático de naturaleza fantástica para los niños',
+        date:'Julio 30 y 31',
+        hour:'11:00 a.m. a 6:00 p.m.',
+        place:'Parque Norte (Estación Universidad)',
+        lat:6.2727881,
+        lng:-75.5703963
+    },
+    {
+        event:'Desfile de autos clásicos y antiguos',
+        date:'Julio 29',
+        hour:'6:00 p.m. a 1:00 a.m',
+        place:'Salida de la sede del periódico El Colombiano',
+        lat:6.1776305,
+        lng:-75.591847
+    },
+    {
+        event:'Desfile de Silleteros',
+        date:'Agosto 7',
+        hour:'2:00 p.m.',
+        place:'Av. Guayabal-Calle 10- Plaza Gardel',
+        lat:6.2181482,
+        lng:-75.5869389
+    },
+    {
+        event:'Orquídeas, Pájaros y Flores',
+        date:'Agosto 3 al 7',
+        hour:'8:00 a. m. a 8:00 p. m',
+        place:'Jardín Botánico',
+        lat:6.2709888,
+        lng:-75.5652386
+    }
+]
